22c399a8a59aa5daf76af4e14383c704f6b80b7a
hsilvioviana <hsilvioviana@hotmail.com>
2021-03-31 11:41:42 -0300

    Adicionado Card dos Produtos

--- src/components/PaginaComprador.js
+++ src/components/PaginaComprador.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import axios from 'axios'
 
 export class PaginaComprador extends React.Component {
+
+    state = {
+        carros: []
+    }
+
+    componentDidMount() {
+        this.pegarCarros()
+    }
+
+    pegarCarros = async () => {
+        try {
+            const response = await axios.get("https://us-central1-labenu-apis.cloudfunctions.net/futureCarThree/cars")
+            this.setState({ carros: response.data.cars })
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
   render() {
-      
+
     return (
       <div>
-        <p>Comprador</p>
+        <section>
+            {this.state.carros.map(carro => {
+                return (
+                    <div>
+                        <h3>Nome: {carro.name}</h3>
+                        <p><b>Descrição:</b> {carro.description}</p>
+                        <p><b>Preço:</b> R${carro.price}</p>
+                        <p><b>Método de Pagamento:</b> {carro.paymentMethod}</p>
+                        <p><b>Prazo de Entrega:</b> {carro.shipping} dias</p><hr/>
+                    </div>
+                )})}
+        </section>
       </div>
     )
   }
