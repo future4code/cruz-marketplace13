8edb876ded5c6bf2f29e8dab1e1374d0f7dd0889
frank159 <frankcf60@gmail.com>
2021-03-31 16:31:24 -0300

    criando footer e header

---
+++ src/components/footer.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from "styled-components"
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+    <FooterDiv>
+			<NomeEmpresa>Futurecar ©</NomeEmpresa>
+			<Atendimento>Atendimento: 9999-9999</Atendimento>
+			<Horario>Segunda a sexta, das 8h às 20h Sábado e feriados, das 8h30 às 15h30</Horario>
+			<Links>Trabalhe com a gente Política de Privacidade Termos de Uso </Links>
+	</FooterDiv>
+        );
+    }
+}
+
+const FooterDiv = styled.div`
+position: absolute;
+width: 1366px;
+height: 221px;
+left: 0px;
+top: 1404px;
+
+background: #495AF6;
+`
+
+const NomeEmpresa = styled.p`
+position: absolute;
+width: 159px;
+height: 33px;
+left: 13px;
+top: 0px;
+
+font-family: 'Poppins', sans-serif;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 37px;
+
+color: #000000;
+
+`
+
+const Atendimento = styled.p`
+position: absolute;
+width: 337px;
+height: 36px;
+left: 13px;
+top: 35px;
+
+font-family: 'Poppins', sans-serif;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 37px;
+
+color: #000000;
+
+`
+
+const Horario = styled.p`
+position: absolute;
+width: 486px;
+height: 81px;
+left: 463px;
+top: 0px;
+
+font-family: 'Poppins', sans-serif;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 37px;
+
+color: #000000;
+`
+const Links = styled.p`
+position: absolute;
+width: 304px;
+height: 187px;
+left: 1062px;
+top: 0px;
+
+font-family: 'Poppins', sans-serif;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 37px;
+
+color: #000000;
+`
