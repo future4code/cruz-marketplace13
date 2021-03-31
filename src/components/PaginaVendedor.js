975c5c2d1a7767117a8a959f34601cc761491baf
hsilvioviana <hsilvioviana@hotmail.com>
2021-03-31 10:42:30 -0300

    Adicionado Pagina Inicial

---
+++ src/components/PaginaVendedor.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class PaginaVendedor extends React.Component {
+  render() {
+    
+    return (
+      <div>
+        <p>Vendedor</p>
+      </div>
+    )
+  }
+}
