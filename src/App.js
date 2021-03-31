d195a5594d8ba6b3cdebdd2a49cf1ee52324ec5e
frank159 <frankcf60@gmail.com>
2021-03-31 16:56:41 -0300

    ajustando footer e mudando a logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { AppContainer } from './components/AppContainer'
+import Footer from './components/footer'
+import Header from './components/header'
 
 function App() {
 	return (
+		<div>
+		<Header />
         <AppContainer />
+		<Footer />
+		</div>
 	)
 }
 
