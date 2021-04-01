390e341dbdc3ffca51cf5f7ec453d6f3f23a3eef
frank159 <frankcf60@gmail.com>
2021-04-01 10:46:29 -0300

    ajustes no header

--- src/components/header.js
+++ src/components/header.js
@@ -7,11 +7,13 @@ export default class Header extends React.Component {
         return (
         <HeaderDiv>
           <Logo src={logo} alt="some text" height="40px" ></Logo>
-            <Menu>
-                <span>Home |</span>
-                <span> vendedor |</span>
-                <span> comprar</span>
-            </Menu>
+            <div>
+                <Home  >Home</Home>
+                <Barra1>|</Barra1>
+                <Vendedor>vendedor</Vendedor>
+                <Barra2>|</Barra2>
+                <Comprador>comprar</Comprador>
+            </div>
          </HeaderDiv>
         );
     }
@@ -40,13 +42,87 @@ line-height: 37px;
 
 color: #FFFFFF;
 `
-const Footer = styled.footer`
+
+const Barra1 = styled.span`
 position: absolute;
-width: 100%;
-height: 221px;
-left: 0px;
-top: 1404px;
+width: 7px;
+height: 36px;
+left: 311px;
+top: 7px;
 
-background: #495AF6;
+font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 37px;
+text-align: right;
 
+color: #FFFFFF;
+
+`
+
+const Barra2 = styled.span`
+position: absolute;
+width: 7px;
+height: 36px;
+left: 453px;
+top: 7px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 37px;
+text-align: right;
+
+color: #FFFFFF;
+`
+
+const Home = styled.span`
+position: absolute;
+width: 76px;
+height: 36px;
+left: 230px;
+top: 7px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 37px;
+text-align: center;
+
+color: #FFFFFF;
+`
+
+const Vendedor = styled.span`
+position: absolute;
+width: 125px;
+height: 36px;
+left: 323px;
+top: 7px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 37px;
+
+color: #FFFFFF;
+`
+
+const Comprador = styled.span`
+position: absolute;
+width: 147px;
+height: 36px;
+left: 465px;
+top: 7px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 37px;
+
+color: #FFFFFF;
 `
